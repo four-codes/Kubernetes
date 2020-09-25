cae22d91cdb8309c0c4c7bead794764b77af6bb9
FourTimes <jjino@protonmail.com>
2020-09-25 16:13:16 +0530

    ReplicaSet Multi

--- minikube.sh
+++ minikube.sh
@@ -2,6 +2,7 @@
 
 sudo curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
   && sudo chmod +x minikube
-sudo mkdir -p /usr/local/bin/
-sudo install minikube /usr/local/bin/
+
+sudo mv minikube /usr/local/bin/
+\
 minikube version
